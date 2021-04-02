a0e619e4cfddb0a7ab77ccc25993d3c7f2914d9c
LIManuel <leonmalisov@gmail.com>
2021-04-01 22:26:26 -0700

    removed middleware config, dropped puma from gemfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,8 +83,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Inserts middleware to perform automatic connection switching.
-  config.middleware.insert_after ActiveRecord::Migration::CheckPending, 
-    "ActiveRecord::ConnectionAdapters::RefreshConnectionManagement"  
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
   # to send a subsequent read to the primary.
