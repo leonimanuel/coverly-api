0205493bf7858ab75788167a9cdb6216567dc954
LIManuel <leonmalisov@gmail.com>
2021-04-03 11:16:32 -0700

    removed omniauth stuff in application.rb

--- config/application.rb
+++ config/application.rb
@@ -46,7 +46,7 @@ module CoverlyApi
     end
 
     # Will this make omniauth work?
-    config.middleware.insert_after ActiveRecord::Migration::CheckPending, ActionDispatch::Cookies
-    config.middleware.insert_after ActionDispatch::Cookies, ActionDispatch::Session::CookieStore    
+    # config.middleware.insert_after ActiveRecord::Migration::CheckPending, ActionDispatch::Cookies
+    # config.middleware.insert_after ActionDispatch::Cookies, ActionDispatch::Session::CookieStore    
   end
 end
