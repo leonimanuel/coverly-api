3e636c151ba419348e00e12297e231fda493c775
LIManuel <leonmalisov@gmail.com>
2021-04-10 12:18:57 -0700

    added comment to blurb import helper

--- app/helpers/blurb_helper.rb
+++ app/helpers/blurb_helper.rb
@@ -1,6 +1,6 @@
 require "spreadsheet"
 
-module BlurbHelper
+module BlurbHelper #run from console with: ApplicationController.helpers.import_blurbs
 	def import_blurbs
 		book = Spreadsheet.open 'company_culture_blurbs.xls'
 		sheet1 = book.worksheet 0 
@@ -10,9 +10,9 @@ module BlurbHelper
 			link = sheet1.row(index)[2]
 			puts "digesting #{company_name}"
 			if company = Company.find_by(name: company_name)
-				company.update(blurb: blurb, link: link)
+				company.update(blurb: blurb, link: link, public: true)
 			elsif company_name
-				company = Company.create(name: company_name, blurb: blurb, link: link)
+				company = Company.create(name: company_name, blurb: blurb, link: link, public: true)
 			end
 		end 			
 	end
