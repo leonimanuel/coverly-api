664ea3bc574840abb6d897f8a80b53cff164eaae
LIManuel <leonmalisov@gmail.com>
2021-06-01 19:11:17 -0700

    configured for pcore test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,55 +12,56 @@ class SessionsController < ApplicationController
 
 	def create
 		# binding.pry
-
-		code = params["code"]
+		puts "YOOOOOOOOOOOOOOOOOOOoooyoyioyiyiiyiyiyiyiyiyiyiyi"
+		puts params
+		# code = params["code"]
 		# redirect_uri = "http://localhost:3000/auth/linkedin/callback"
-		redirect_uri = "https://coverly-api.herokuapp.com/auth/linkedin/callback"
+		# # redirect_uri = "https://coverly-api.herokuapp.com/auth/linkedin/callback"
 
-		uri = URI("https://www.linkedin.com/oauth/v2/accessToken")
-		res = Net::HTTP.post_form(uri, 
-		"grant_type" => "authorization_code",
-		"code" => code,
-		"redirect_uri" => redirect_uri,
-		"client_id" => ENV['LINKEDIN_KEY'],
-		"client_secret" => ENV['LINKEDIN_SECRET']
-		)
+		# uri = URI("https://www.linkedin.com/oauth/v2/accessToken")
+		# res = Net::HTTP.post_form(uri, 
+		# "grant_type" => "authorization_code",
+		# "code" => code,
+		# "redirect_uri" => redirect_uri,
+		# "client_id" => ENV['LINKEDIN_KEY'],
+		# "client_secret" => ENV['LINKEDIN_SECRET']
+		# )
 
-		token = JSON.parse(res.body)["access_token"]
-		# token = "AQV2gW1DYMp7NEhdun_PJ8Js_dd2LaDD0TGm6pcAzxepdotdhRHP1zncXEhPe1F4vCvXKcsDowV9TwHYiz0ZIxO36X4IdtFAWQYprWDiay74p5CXb2RhLgxBfsJ988ahelKjfysIDxXoSmFwx-UmoavFLu4SpJhDexiWQPciWp2CCnsAwXOC7SoTU9oybmbcY3JwYMhiAV7REsqZ98nh1y6uexlyodh8yaIkmBvyeM_6fFdvRlBEq6QPscSsyMw8qy0C-224WELZ1gVqkqCPfM3ApCRKe74fwgi3nF-Gv_UUE2NESfJe1bCYc9WATlgyU_ElrLEYhhs5Fmoy3GSBA1xNiN7-4g"
+		# token = JSON.parse(res.body)["access_token"]
+		# # token = "AQV2gW1DYMp7NEhdun_PJ8Js_dd2LaDD0TGm6pcAzxepdotdhRHP1zncXEhPe1F4vCvXKcsDowV9TwHYiz0ZIxO36X4IdtFAWQYprWDiay74p5CXb2RhLgxBfsJ988ahelKjfysIDxXoSmFwx-UmoavFLu4SpJhDexiWQPciWp2CCnsAwXOC7SoTU9oybmbcY3JwYMhiAV7REsqZ98nh1y6uexlyodh8yaIkmBvyeM_6fFdvRlBEq6QPscSsyMw8qy0C-224WELZ1gVqkqCPfM3ApCRKe74fwgi3nF-Gv_UUE2NESfJe1bCYc9WATlgyU_ElrLEYhhs5Fmoy3GSBA1xNiN7-4g"
 
-		# url = 'https://api.linkedin.com/v2/me'
-		url = 'https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))'
-		headers = {
-		  Authorization: "Bearer #{token}",
-		}
+		# # url = 'https://api.linkedin.com/v2/me'
+		# url = 'https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))'
+		# headers = {
+		#   Authorization: "Bearer #{token}",
+		# }
 
-		response = HTTParty.get(url, headers: headers)
+		# response = HTTParty.get(url, headers: headers)
 
-		email = response["elements"][0]["handle~"]["emailAddress"]
+		# email = response["elements"][0]["handle~"]["emailAddress"]
 
-		random_string = SecureRandom.urlsafe_base64
+		# random_string = SecureRandom.urlsafe_base64
 
-		if !User.where(email: email).empty? #if user exists
-			user = User.find_by(email: email)
-			user.update(password: random_string)
-		else
-			url = 'https://api.linkedin.com/v2/me'
-			response = HTTParty.get(url, headers: headers)
-			first_name = response["localizedFirstName"]
-			last_name = response["localizedLastName"]
+		# if !User.where(email: email).empty? #if user exists
+		# 	user = User.find_by(email: email)
+		# 	user.update(password: random_string)
+		# else
+		# 	url = 'https://api.linkedin.com/v2/me'
+		# 	response = HTTParty.get(url, headers: headers)
+		# 	first_name = response["localizedFirstName"]
+		# 	last_name = response["localizedLastName"]
 
-			user = User.create(
-				first_name: first_name, 
-				last_name: last_name,
-				email: email,
-				password: random_string)
-		end
+		# 	user = User.create(
+		# 		first_name: first_name, 
+		# 		last_name: last_name,
+		# 		email: email,
+		# 		password: random_string)
+		# end
 
-		command = AuthenticateUser.call(user.email, user.password)
+		# command = AuthenticateUser.call(user.email, user.password)
 
 		# redirect_to "http://localhost:3001/auth?token=#{command.result}"		
-		redirect_to "https://coverly.io/auth?token=#{command.result}"		
+		# redirect_to "https://coverly.io/auth?token=#{command.result}"		
 	end
 
 	def test
