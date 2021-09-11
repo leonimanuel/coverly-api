94224120d8e62134d62db321b071a3cccbe5baf8
LIManuel <leonmalisov@gmail.com>
2021-09-11 18:21:53 -0400

    refactor initial general keyword fetch for performance

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -2,15 +2,7 @@ class KeywordsController < ApplicationController
 	skip_before_action :authenticate_request
 
 	def general
-		# company = Company.find(params["id"])
-		general = Industry.find_by(name: "General")
-		keywords = []
-		Keyword.all.each do |kw| 
-			if kw.industries.include?(general)
-				keywords << kw
-			end
-		end
-		# keywords = Keyword.where(industry: general, public: true)
+		keywords = Keyword.includes(:industries).where('industries.name = ?', 'General').references(:industries)
 		render json: keywords
 	end
 end
--- app/serializers/position_serializer.rb
+++ app/serializers/position_serializer.rb
@@ -1,7 +1,11 @@
 class PositionSerializer < ActiveModel::Serializer
-  attributes :id, :name, :keywords, :industry_keywords
+  attributes :id, :name, :keywords, :industry, :industry_keywords
 
   def industry_keywords
   	object.industry.keywords.where(public: true)
+  end
+
+  def industry
+  	object.industry.name
   end
 end
